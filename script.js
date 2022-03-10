3b744fef5a05e34e20d9b3b111132d96f3c5a039
shaden <92798768+shadenGhasri@users.noreply.github.com>
2022-03-10 21:51:36 +0330

    add

--- script.js
+++ script.js
@@ -1,16 +1,29 @@
+const btnJoke = document.querySelector(".btn-joke")
+const ulJoke = document.querySelector(".ul-joke")
 
+const createJoke = async ()=>{
+    const res =await getData()
+    console.log(res)
+    const li = document.createElement("li")
+    li.textContent =    `${res}`
+    ulJoke.append(li)
 
-
+}
 const getData = async ()=>{
     try{
         const config = {headers:{Accept :"application/json"}}
-        const response = await axios("https://icanhazdadjoke.com/",config)
-        const joke = response.data.joke
-        console.log(joke)
+        const response = await axios.get("https://icanhazdadjoke.com/",config)
+        return response.data.joke
+        
     }catch(error){
         console.error(error)
     }
 }
+
+
+
+
+btnJoke.addEventListener("click",createJoke)
 
 
 
